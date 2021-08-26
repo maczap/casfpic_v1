22f62e8d61aeecffcf0aec6ce935d37d12314a95
maczap <kinho2000@gmail.com>
2021-08-26 14:00:47 -0300

    up

--- resources/views/layouts/success.blade.php
+++ resources/views/layouts/success.blade.php
@@ -10,15 +10,17 @@
                         <h2 class='pgh2smart' style="margin-bottom:40px;">Pagamento efetuado com sucesso</h2>
                         
                         <p style="font-weight:normal;"> <strong>{{$nome}}</strong>, a ativação do seu plano <strong> {{$plano}}</strong> foi concluída com sucesso.</br>Desejamos boas-vindas à CASFPIC Que bom ter você com a gente!</p>
-                        <p style="font-weight:normal;" >Para utilizar seu plano, basta seguir o passo a passo abaixo: </p>
+                        <p>A partir do 1° dia do mês subsequente você já poderá usufruir do primeiro benefício de Assistência Odontológica Privada com base nos regulamentos junto ANS – AGÊNCIA NACIONAL DE SAÚDE SUPLEMENTAR – www.ans.gov.br</p>
+                        <p>Até o ultimo dia do mês vigente receberá via correio eletrônico ou via postal as seguintes informações:</p>
 
                             <ul>
-                                <li>Passo 1</li>
-                                <li>Passo 2</li>
-                                <li>Passo 3</li>
-                                <li>Passo 4</li>
-                                <li>Passo 5</li>
-                            </ul>                        
+                                <li>1. Documento de identificação (carteirinha) eletrônica ou plástica.</li>
+                                <li>2. Garantias de atendimentos conforme a Adesão Associativa, digo, BRONZE, PRATA, OURO E DIAMANTE – www.casfpic.org.br</li>
+                                <li>3. Rede credenciada dos profissionais disponíveis na sua cidade ou região na qual a sua utilização é sem carências e em conformidade com os agendamentos dos profissionais credenciados, ratificando que o atendimento eletivo é em âmbito NACIONAL.</li>
+                                <li>4. Boleto ou outras formas de pagamento caso a sua opção for mensal.</li>
+                                
+                            </ul>  
+                            <p>O estatuto e as regras associativas da caixa estão disponíveis no nosso website www.casfpic.org.br</p>                      
                         </div>
                     </div>
 
