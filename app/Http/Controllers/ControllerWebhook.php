40d012c67c755eb6ab11abd2b584d7b0d9227c2c
Marcos Grnziera <kinho2000@gmail.com>
2021-11-12 10:06:58 -0300

    up

--- app/Http/Controllers/ControllerWebhook.php
+++ app/Http/Controllers/ControllerWebhook.php
@@ -17,7 +17,7 @@ class ControllerWebhook extends Controller
             // ]);
             $dados = Webhook::create([
                 // 'hooks' => json_encode($request->all()),
-                'hooks' => $retorno["signature"]
+                'hooks' => $retorno[0]
                 
             ]);  
 
