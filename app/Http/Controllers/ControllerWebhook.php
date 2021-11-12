62be08fae653e8632297b8af8a87eeb3732d136f
Marcos Grnziera <kinho2000@gmail.com>
2021-11-12 10:08:39 -0300

    up

--- app/Http/Controllers/ControllerWebhook.php
+++ app/Http/Controllers/ControllerWebhook.php
@@ -10,7 +10,7 @@ class ControllerWebhook extends Controller
     public function webhooks(Request $request){
 
             // $retorno = json_encode($request->all());
-            $retorno = $request->all();
+            $retorno = $request[0];
         
             // DB::table('webhooks')->insert([
             //     'hooks' => json_encode($request->all())
